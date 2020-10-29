a154092f23ad6f90aee3dcfd4b7c262a9b21c24b
Zachary Sweigart <zsweigart@plaid.com>
2020-10-29 15:33:13 -0500

    Move to one file

--- types/Types.d.ts
+++ types/Types.d.ts
@@ -128,3 +128,34 @@ export enum LinkAccountSubtype {
     UTMA = 'utma',
     VARIABLE_ANNUITY = 'variable annuity',
 }
+export interface LinkSuccess {
+    publicToken: string;
+    metadata: LinkSuccessMetadata;
+}
+
+export interface LinkSuccessMetadata {
+    institution?: LinkInstitution;
+    accounts: LinkAccount[];
+    linkSessionId: String;
+    metadataJson: String;
+}
+
+export interface LinkAccount {
+    id: String;
+    name?: String;
+    mask?: String;
+    type: LinkAccountType;
+    subtype: LinkAccountSubtype;
+    verificationStatus: LinkAccountVerificationStatus;
+}
+
+export enum LinkAccountVerificationStatus {
+    PENDING_AUTOMATIC_VERIFICATION = 'pending_automatic_verification',
+    PENDING_MANUAL_VERIFICATION = 'pending_manual_verification',
+    MANUALLY_VERIFIED = 'manually_verified',
+}
+
+export interface LinkInstitution {
+    id: String;
+    name: String;
+}

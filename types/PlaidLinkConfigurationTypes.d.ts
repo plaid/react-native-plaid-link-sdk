ad4df9c6120f4e75a0594dfd00ddd9a6adecc188
Zachary Sweigart <zsweigart@plaid.com>
2020-10-29 14:53:44 -0500

    Moved common fields to common type

--- types/PlaidLinkConfigurationTypes.d.ts
+++ types/PlaidLinkConfigurationTypes.d.ts
@@ -3,12 +3,12 @@ import { LinkAccountSubtype } from "./AccountSubtypes";
 interface CommonPlaidLinkOptions {
     onSuccess: Function;
     onExit?: Function;
+    logLevel?: LinkLogLevel;
+    extraParams?: Record<string, any>;
   }
 
   export type LinkTokenConfiguration = (CommonPlaidLinkOptions & {
     token: string; 
-    logLevel?: LinkLogLevel;
-    extraParams?: Record<string, any>;
   });
 
   export type LinkPublicKeyConfiguration = (CommonPlaidLinkOptions & {
@@ -27,8 +27,6 @@ interface CommonPlaidLinkOptions {
     oauthNonce?: string;
     oauthRedirectUri?: string;
     oauthStateId?: string;
-    logLevel?: LinkLogLevel;
-    extraParams?: Record<string, any>;
   });
    
   export enum LinkLogLevel {

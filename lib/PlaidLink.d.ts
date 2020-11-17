b4d650c983721a27fef534e2542b555efc2464cf
Zachary Sweigart <zsweigart@plaid.com>
2020-11-17 14:53:30 -0600

    tsc

---
+++ lib/PlaidLink.d.ts
@@ -0,0 +1,13 @@
+/// <reference types="react" />
+import { LinkEventListener, PlaidLinkProps } from './Types';
+/**
+ * A hook that registers a listener on the plaid emitter for the 'onEvent' type.
+ * The listener is cleaned up when this view is unmounted
+ *
+ * @param LinkEventListener the listener to call
+ */
+export declare const usePlaidEmitter: (LinkEventListener: LinkEventListener) => void;
+export declare const openLink: (props: PlaidLinkProps) => Promise<void>;
+export declare const dismissLink: () => void;
+export declare const useDeepLinkRedirector: () => void;
+export declare const PlaidLink: (props: PlaidLinkProps) => JSX.Element;

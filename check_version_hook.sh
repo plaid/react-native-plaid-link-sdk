c951e5a4ede89449dfcb0f3e8c2ab36b62f85e8d
Jeroen Mols <jeroen@jeroenmols.com>
2020-12-04 15:28:41 +0100

    convert hook to javascript

--- check_version_hook.sh
+++
@@ -1,9 +0,0 @@
-# Check for version alignment between RN and Android SDK
-rn_version=$(awk '/"version"/ {print $2;exit}' package.json | tr -d '",')
-android_version=$(awk -F'"' '/android:value/ {print $2}' android/src/main/AndroidManifest.xml)
-
-if [ "${rn_version}" != "$android_version" ]; then
-  echo "Commit failed SDK version mismatch"
-  echo "Please ensure package.json and android/src/main/AndroidManifest.xml have the same version"
-  exit -1
-fi

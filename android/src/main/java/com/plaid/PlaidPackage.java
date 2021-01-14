ce9b197cf126c97e4ff4fef0878851fa7e6fb5d9
Jeroen Mols <jeroen@jeroenmols.com>
2021-01-14 09:14:10 +0100

    replace turbo package with standard package

--- android/src/main/java/com/plaid/PlaidPackage.java
+++ android/src/main/java/com/plaid/PlaidPackage.java
@@ -1,38 +1,30 @@
 package com.plaid;
 
-import java.util.HashMap;
-import java.util.Map;
+import androidx.annotation.NonNull;
 
-import com.facebook.react.TurboReactPackage;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.module.model.ReactModuleInfo;
-import com.facebook.react.module.model.ReactModuleInfoProvider;
+import com.facebook.react.uimanager.ViewManager;
 
 @SuppressWarnings("unused")
-public class PlaidPackage extends TurboReactPackage {
+public class PlaidPackage implements ReactPackage {
 
+  @NonNull
   @Override
-  public NativeModule getModule(
-      String name, ReactApplicationContext reactContext) {
-    return new PlaidModule(reactContext);
+  public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactContext) {
+    List<NativeModule> modules = new ArrayList<>();
+    modules.add(new PlaidModule(reactContext));
+    return modules;
   }
 
+  @NonNull
   @Override
-  public ReactModuleInfoProvider getReactModuleInfoProvider() {
-    return () -> {
-      Map<String, ReactModuleInfo> map = new HashMap<>();
-      map.put(
-          "PlaidAndroid",
-          new ReactModuleInfo(
-              "PlaidAndroid",
-              "com.reactlibrary.PlaidModule",
-              false,
-              false,
-              true,
-              false,
-              false));
-      return map;
-    };
+  public List<ViewManager> createViewManagers(@NonNull ReactApplicationContext reactContext) {
+    return Collections.emptyList();
   }
 }

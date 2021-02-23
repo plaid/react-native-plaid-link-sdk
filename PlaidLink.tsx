ac3e67eb8ecdc362732a01d8bb297bb57b00e99c
Jeroen Mols <jeroen@jeroenmols.com>
2021-02-23 15:45:08 +0100

    Update deprectation link

--- PlaidLink.tsx
+++ PlaidLink.tsx
@@ -39,7 +39,7 @@ export const usePlaidEmitter = (LinkEventListener: LinkEventListener) => {
 
 export const openLink = async (props: PlaidLinkProps) => {
   if (props.tokenConfig == null) {
-    console.log('The public_key is being deprecated. Learn how to upgrade to link_tokens at https://plaid.com/docs/#create-link-token')
+    console.log('The public_key is being deprecated. Learn how to upgrade to link_tokens at https://plaid.com/docs/link-token-migration-guide/')
   }
   let config = props.tokenConfig ? props.tokenConfig : props.publicKeyConfig!;
 

2d88748cc0de6cffeb3ec86d233626c465aab9db
Zachary Sweigart <zsweigart@gmail.com>
2021-05-04 09:10:12 -0500

    Update RNLinksdk.m

--- ios/RNLinksdk.m
+++ ios/RNLinksdk.m
@@ -53,7 +53,7 @@ NSString* const kRNLinkKitDepositSwitchTokenPrefix = @"deposit-switch-";
 RCT_EXPORT_MODULE();
 
 + (NSString*)sdkVersion {
-return @"7.0.4"; // SDK_VERSION
+    return @"7.0.4"; // SDK_VERSION
 }
 
 + (NSString*)objCBridgeVersion {

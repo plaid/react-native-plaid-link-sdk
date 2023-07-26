42e69e4c1a5387ed4ff4f8399f26b29072ba9c7d
dtroupe <dtroupe@plaid.com>
2023-07-26 16:36:16 -0700

    updates

--- example/ios/plaidRNDemoTests/PlaidCredentials.swift
+++ example/ios/plaidRNDemoTests/PlaidCredentials.swift
@@ -1,5 +1,8 @@
 import Foundation
 
+// Run git update-index --skip-worktree example/ios/plaidRNDemoTests/PlaidCredentials.swift
+// before modifying this file to prevent git from picking up any changes.
+
 struct PlaidCredentials {
     static let clientID = "YOUR_CLIENT_ID"
     static let clientSecret = "YOUR_CLIENT_SECRET"
--- example/ios/plaidRNDemoTests/create_credentials_swift_struct.rb
+++ example/ios/plaidRNDemoTests/create_credentials_swift_struct.rb
@@ -8,6 +8,11 @@ File.readlines("plaidRNDemoTests/env-vars.txt").each do |line|
 end
 
 file_content = <<-CREDS_FILE_STRING
+import Foundation
+
+// Run git update-index --skip-worktree example/ios/plaidRNDemoTests/PlaidCredentials.swift
+// before modifying this file to prevent git from picking up any changes.
+
 struct PlaidCredentials {
     static let clientID = "#{ENV['PLAID_CLIENT_ID']}"
     static let clientSecret = "#{ENV['PLAID_SECRET']}"

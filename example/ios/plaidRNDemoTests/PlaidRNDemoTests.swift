0363cdbc390659595e62aaf33e97e3038d8145c6
dtroupe <dtroupe@plaid.com>
2023-07-27 08:46:57 -0700

    testing updates

--- example/ios/plaidRNDemoTests/PlaidRNDemoTests.swift
+++ example/ios/plaidRNDemoTests/PlaidRNDemoTests.swift
@@ -4,9 +4,20 @@ import LinkKit
 final class PlaidRNDemoTests: XCTestCase {
 
   func testVersion() {
-
     let version = LinkKitVersion
 
     XCTAssertEqual(version, "4.4.0")
+  }
+
+  func testPlaidCredentialsExist() {
+    let clientID = PlaidCredentials.clientID
+
+    XCTAssertNotEqual(clientID, "YOUR_CLIENT_ID")
+    XCTAssertFalse(clientID.contains("*"))
+
+    let secret = PlaidCredentials.clientSecret
+
+    XCTAssertNotEqual(secret, "YOUR_CLIENT_SECRET")
+    XCTAssertFalse(secret.contains("*"))
   }
 }
--- example/ios/plaidRNDemoTests/create_credentials_swift_struct.rb
+++ example/ios/plaidRNDemoTests/create_credentials_swift_struct.rb
@@ -12,6 +12,12 @@ if(CLIENT_ID != nil and SECRET != nil)
   //
   // Enter your client id and secret into this file.
   // Then you can run the unit tests for the iOS project.
+  //
+  // After making changes you'll need to run
+  // git update-index --no-skip-worktree example/ios/plaidRNDemoTests/PlaidCredentials.swift
+  // git git stash -- example/ios/plaidRNDemoTests/PlaidCredentials.swift
+  //
+  // Before you can switch branches.
 
   struct PlaidCredentials {
       static let clientID = "#{CLIENT_ID}"

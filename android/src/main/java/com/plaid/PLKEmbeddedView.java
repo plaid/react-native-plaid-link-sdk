d944735559c07e3a0d93b0df02ad75273aaa3c4a
dtroupe <dtroupe@plaid.com>
2023-08-17 11:37:59 -0700

    get events working

--- android/src/main/java/com/plaid/PLKEmbeddedView.java
+++ android/src/main/java/com/plaid/PLKEmbeddedView.java
@@ -2,22 +2,37 @@ package com.plaid;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.events.RCTEventEmitter;
 
 public class PLKEmbeddedView extends LinearLayout {
 
-    public PLKEmbeddedView(Context context) {
+    private ThemedReactContext themedReactContext;
+    private String token = "";
+
+    public PLKEmbeddedView(ThemedReactContext context) {
         super(context);
+        this.themedReactContext = context;
         initView();
     }
 
-    public PLKEmbeddedView(Context context, AttributeSet attrs, int defStyle) {
+    public PLKEmbeddedView(ThemedReactContext context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        this.themedReactContext = context;
         initView();
     }
 
-    public PLKEmbeddedView(Context context, AttributeSet attrs) {
+    public PLKEmbeddedView(ThemedReactContext context, AttributeSet attrs) {
         super(context, attrs);
+        this.themedReactContext = context;
         initView();
     }
 
@@ -25,7 +40,33 @@ public class PLKEmbeddedView extends LinearLayout {
         super.onFinishInflate();
     }
 
+    public void setToken(String token) {
+        this.token = token;
+
+        TextView textView = findViewById(R.id.randomNumber);
+        textView.setText("Token " + this.token);
+    }
+
     private void initView() {
         inflate(getContext(), R.layout.plk_embedded_view, this);
+
+        Button button = findViewById(R.id.randomButton);
+        button.setOnClickListener(onClickListener);
     }
+
+    private OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            WritableMap metadataMap = Arguments.createMap();
+            metadataMap.putString("linkSessionId", "abc123");
+            metadataMap.putString("viewName", "CONSENT");
+
+            WritableMap event = Arguments.createMap();
+            event.putString("eventName","TRANSITION_VIEW");
+            event.putMap("metadata", metadataMap);
+
+            ReactContext reactContext = (ReactContext)getContext();
+            reactContext.getJSModule(RCTEventEmitter.class).receiveEvent(getId(), "onEventEvent", event);
+        }
+    };
 }
--- android/src/main/java/com/plaid/PLKEmbeddedViewManager.java
+++ android/src/main/java/com/plaid/PLKEmbeddedViewManager.java
@@ -3,14 +3,16 @@ package com.plaid;
 import android.content.Context;
 import android.view.LayoutInflater;
 
+import com.facebook.react.common.MapBuilder;
 import com.facebook.react.uimanager.SimpleViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
+import com.facebook.react.uimanager.annotations.ReactProp;
+
+import java.util.Map;
 
 public class PLKEmbeddedViewManager extends SimpleViewManager<PLKEmbeddedView> {
 
     public static final String REACT_CLASS = "PLKEmbeddedView";
-    String eventName = "onClick";
-
     @Override
     public String getName() { return REACT_CLASS; }
 
@@ -18,5 +20,20 @@ public class PLKEmbeddedViewManager extends SimpleViewManager<PLKEmbeddedView> {
     public PLKEmbeddedView createViewInstance(ThemedReactContext context) {
         PLKEmbeddedView view = new PLKEmbeddedView(context);
         return  view;
+    }
+
+    @ReactProp(name ="token")
+    public void setToken(PLKEmbeddedView view, String token) {
+        view.setToken(token);
+    }
+
+    public Map getExportedCustomBubblingEventTypeConstants() {
+        String eventName = "onEventEvent";
+        String propName = "onEvent";
+        Map onClickHandler = MapBuilder.of("phasedRegistrationNames",MapBuilder.of("bubbled", propName));
+
+        MapBuilder.Builder events = MapBuilder.builder();
+        events.put(eventName, onClickHandler); // qwe rename
+        return events.build();
     }
 }

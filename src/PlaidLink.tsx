690c1c0a5483bcb1a65d7a4956900512f78977a6
dtroupe <dtroupe@plaid.com>
2023-10-16 13:44:33 -0700

    remove public key

--- src/PlaidLink.tsx
+++ src/PlaidLink.tsx
@@ -42,7 +42,7 @@ export const openLink = async (props: PlaidLinkProps) => {
   if (props.tokenConfig == null) {
     console.log('The public_key is being deprecated. Learn how to upgrade to link_tokens at https://plaid.com/docs/link-token-migration-guide/')
   }
-  let config = props.tokenConfig ? props.tokenConfig : props.publicKeyConfig!;
+  let config = props.tokenConfig;
 
   if (Platform.OS === 'android') {
     NativeModules.PlaidAndroid.startLinkActivityForResult(
@@ -63,7 +63,7 @@ export const openLink = async (props: PlaidLinkProps) => {
       },
     );
   } else {
-    NativeModules.RNLinksdk.create(config);
+    NativeModules.RNLinksdk.create(config.token, config.noLoadingState);
 
     let presentFullScreen = props.iOSPresentationStyle == LinkIOSPresentationStyle.FULL_SCREEN
 

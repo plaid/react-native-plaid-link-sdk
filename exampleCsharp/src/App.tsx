7dab78eeff2c75bc6cdff02086ff2514d78a9d7a
Aimee <alramirez1@alumni.fullsail.edu>
2024-09-02 17:03:09 -0400

    simple example for requesting

--- exampleCsharp/src/App.tsx
+++ exampleCsharp/src/App.tsx
@@ -1,42 +1,39 @@
-// App.tsx
+
+
+
 import * as React from 'react';
-import { NativeModules,Button, Text, View, ActivityIndicator, StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { PlaidEmbeddedLinkScreen } from './Screens/PlaidEmbeddedLinkScreen';
-import { PlaidLinkScreen } from './Screens/PlaidLinkScreen';
-// import PlaidIntergrationComponent from './Screens/PlaidIntergrationComponent'; // Ensure the correct path and spelling
-import { styles } from './Styles'; // Styles used throughout the app
-const { NavigationModule,  PlaidClassModule } = NativeModules;
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { NativeModules} from 'react-native';
+import {PlaidEmbeddedLinkScreen} from './Screens/PlaidEmbeddedLinkScreen';
+import {PlaidLinkScreen} from './Screens/PlaidLinkScreen';
 
-console.log(NavigationModule)
-// Export the functions to use in your app
-export const navigateToCustomWebViewPage = () => {
-  console.log('WebView')
-
-  NavigationModule.navigateToCustomWebViewPage();
-};
-
-export const navigateBackToApp = () => {
-  console.log('App')
-  NavigationModule.navigateBackToApp();
-};
-
-// Define the tab navigator
+const { PlaidClassModule } = NativeModules;
 const Tab = createBottomTabNavigator();
-
 
 export default function App() {
   // State for managing the link token
-  const [token, setToken] = React.useState('');
+  const [linkToken, setLinkToken] = React.useState('');
+
+  const [publicToken, setPublicToken] = React.useState('');
   const [loading, setLoading] = React.useState(true); // State to manage loading status
 
   // Function to retrieve the link token from the native module
   const getLinkToken = async () => {
     try {
       const linkToken = await PlaidClassModule.getLinkToken();
-      PlaidClassModule.PLAID_PUBLIC_TOKEN = linkToken; // Set the token in the native module
-      setToken(linkToken); // Update the state with the fetched token
+      PlaidClassModule.PLAID_LINK_TOKEN = linkToken; // Set the token in the native module
+      setLinkToken(linkToken); 
+      if(linkToken){
+        const publicToken = await PlaidClassModule.getPublicToken();
+        PlaidClassModule.PLAID_PUBLIC_TOKEN = publicToken; 
+        setPublicToken(publicToken);
+        if(publicToken){
+          const accessToken = await PlaidClassModule.exchangePublicToken();
+          PlaidClassModule.PLAID_ACCESS_TOKEN = accessToken; 
+        }
+        // Set the token in the native module
+      }// Update the state with the fetched token
       setLoading(false); // Stop the loading indicator
     } catch (error) {
       console.error('Error fetching the link token:', error);
@@ -50,45 +47,18 @@ export default function App() {
   }, []);
 
   return (
-    <View style={styles.appContainer}>
-      {/* Display the token or a loading indicator */}
-      {loading ? (
-        <ActivityIndicator size="large" color="#0000ff" style={styles.loadingIndicator} />
-      ) : (
-        <>
-          <Text style={styles.tokenText}>{token ? `Token: ${token}` : 'No token available'}</Text>
-          <View style={styles.container}>
-      <Button
-        title="Go to Custom Web View"
-        onPress={navigateToCustomWebViewPage}
-      />
-      <Button
-        title="Back to App"
-        onPress={navigateBackToApp}
-      />
-    </View>
-          {/* Integration Component with the Plaid Link */}
-          {/* <PlaidIntergrationComponent token={token} /> */}
-
-          {/* Navigation Container with Tab Navigator */}
-          <NavigationContainer>
-            <Tab.Navigator
-              screenOptions={{
-                tabBarLabelPosition: 'beside-icon',
-                tabBarLabelStyle: {
-                  fontWeight: '700',
-                  fontSize: 15,
-                },
-                tabBarIconStyle: { display: 'none' },
-              }}
-            >
-              <Tab.Screen name="Standard Link" component={PlaidLinkScreen} />
-              <Tab.Screen name="Embedded Link" component={PlaidEmbeddedLinkScreen} />
-            </Tab.Navigator>
-          </NavigationContainer>
-        </>
-      )}
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{
+          tabBarLabelPosition: 'beside-icon',
+          tabBarLabelStyle: {
+            fontWeight: '700',
+            fontSize: 15,
+          },
+          tabBarIconStyle: {display: 'none'},
+        }}>
+        <Tab.Screen name="Standard Link" component={PlaidLinkScreen} />
+        <Tab.Screen name="Embedded Link" component={PlaidEmbeddedLinkScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
--- exampleCsharp/windows/PlaidReactNativeDemo/Modules/PlaidLinkModule.cs
+++ exampleCsharp/windows/PlaidReactNativeDemo/Modules/PlaidLinkModule.cs
@@ -1,24 +1,36 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.ReactNative;
 using Microsoft.ReactNative.Managed;
-using System.Net.Http;
 using Newtonsoft.Json;
-using Windows.Management;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Core;
+using Microsoft.ReactNative;
+using PlaidReactNativeDemo;
 
 namespace PlaidLinkModuleWindows
 {
     [ReactModule("PlaidClassModule")]
     public sealed class PlaidClassModule
     {
+        public WebView PlaidWebView => PlaidWebView;
+        public ReactRootView ReactRootView => reactRootView;
+
+        private ReactRootView reactRootView;
+        private CoreDispatcher dispatcher = Windows.UI.Xaml.Window.Current.Dispatcher;
+
         // Constants exposed to JavaScript
+        [ReactConstant("PLAID_LINK_TOKEN")]
+        public string _linkToken = "link-";
+
         [ReactConstant("PLAID_PUBLIC_TOKEN")]
         public string _publicToken = "public-";
+
         [ReactConstant("PLAID_ACCESS_TOKEN")]
         public string _accessToken = "access-";
+
         [ReactConstant("PLAID_BACKEND_URL")]
         public string backendApiUrl = "https://greenfieldxplatform-00f7d0070a43.herokuapp.com";
 
@@ -47,9 +59,10 @@ namespace PlaidLinkModuleWindows
                     string data = await response.Content.ReadAsStringAsync();
                     dynamic jsonData = JsonConvert.DeserializeObject(data);
                     string linkToken = jsonData.link_token;
-                    _publicToken = linkToken;
-                    _ = ExchangePublicTokenAsync(_publicToken);
-                    return linkToken;
+                    _linkToken = linkToken;
+
+                    var returnObject = new { linkToken = _linkToken, metadata = jsonData };
+                    return JsonConvert.SerializeObject(returnObject, Formatting.None);
                 }
             }
             catch (Exception ex)
@@ -63,25 +76,51 @@ namespace PlaidLinkModuleWindows
         {
             try
             {
-                
                 using (HttpClient client = new HttpClient())
                 {
-                    var requestBody = new { public_token =  _publicToken };
-
+                    var requestBody = new { public_token = _publicToken };
                     var json = JsonConvert.SerializeObject(requestBody);
+                    var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-                    var content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
                     HttpResponseMessage response = await client.PostAsync($"{backendApiUrl}/plaid/api/exchange_public_token", content);
                     response.EnsureSuccessStatusCode();
 
                     string responseData = await response.Content.ReadAsStringAsync();
-
                     dynamic jsonData = JsonConvert.DeserializeObject(responseData);
                     string accessToken = jsonData.access_token;
-                    
-                    _accessToken = accessToken;
 
+                    _accessToken = accessToken;
                     return _accessToken;
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        ///
+        // Method to get a public token by making a request to Plaid
+        [ReactMethod("getPublicToken")]
+        public async Task<string> GetPublicTokenAsync(string token)
+        {
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    var requestBody = new { public_token = publicToken };
+                    var json = JsonConvert.SerializeObject(requestBody);
+                    var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                    HttpResponseMessage response = await client.PostAsync($"{backendApiUrl}/plaid/api/create_public_token", content);
+                    response.EnsureSuccessStatusCode();
+
+                    string responseData = await response.Content.ReadAsStringAsync();
+                    dynamic jsonData = JsonConvert.DeserializeObject(responseData);
+                    string publicToken = jsonData.access_token;
+
+                    _publicToken = publicToken;
+                    return _publicToken;
                 }
             }
             catch (Exception ex)

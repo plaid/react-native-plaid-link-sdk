4f1ab012b0a9e1cf718de186b0c7b5cb4a31e229
dtroupe <dtroupe@plaid.com>
2026-03-25 10:09:30 -0700

    add create session v1

--- ios/src/ReactNativePlaidLinkSdkModule.swift
+++ ios/src/ReactNativePlaidLinkSdkModule.swift
@@ -2,19 +2,48 @@ import ExpoModulesCore
 internal import LinkKit
 
 public class ReactNativePlaidLinkSdkModule: Module {
+
     // Each module class must implement the definition function. The definition consists of components
     // that describes the module's functionality and behavior.
     // See https://docs.expo.dev/modules/module-api for more details about available components.
     public func definition() -> ModuleDefinition {
+
         // Sets the name of the module that JavaScript code will use to refer to the module. Takes a string as an argument.
         // Can be inferred from module's class name, but it's recommended to set it explicitly for clarity.
         // The module will be accessible from `requireNativeModule('ReactNativePlaidLinkSdk')` in JavaScript.
         Name("ReactNativePlaidLinkSdk")
 
         // --- Version API ---
-//        Constants([
-//            "sdkVersion": Plaid.version
-//        ])
+        let sdkVersion = Constant("sdkVersion", get: { Plaid.version })
+
+
+        AsyncFunction(ModuleFunctionName.createPlaidLinkSession.rawValue) { (token: String, onLoadPromise: Promise) in
+            let onSuccess: OnSuccessHandler = { [weak self] success in
+                self?.sendEvent(ModuleEventName.onSuccess.rawValue, ["onSuccess": "fixme"])
+            }
+
+            let onExit: OnExitHandler = { [weak self] exit in
+                self?.sendEvent(ModuleEventName.onExit.rawValue, ["onExit": "fixme"])
+                self.linkSession = nil
+            }
+
+            let onEvent: OnEventHandler = { [weak self] event in
+                self?.sendEvent(ModuleEventName.onEvent.rawValue, ["onEvent": event.eventName.description]) // fixme!
+            }
+
+            let onLoad: OnLoadHandler = {
+                onLoadPromise.resolve()
+            }
+
+            let config = LinkTokenConfiguration(token: token, onSuccess: onSuccess, onExit: onExit, onLoad: onLoad)
+            do {
+                let session = try Plaid.createPlaidLinkSession(configuration: config)
+                self.linkSession = session
+            } catch {
+                self.sessionCreationError = error
+                onLoadPromise.reject("LINK_SESSION_CREATE_ERROR", error.localizedDescription)
+            }
+        }
 //
 //        // --- Create Link Session API ---
 //        // This receives a dictionary from JS and maps it to your Swift Configuration
@@ -59,7 +88,9 @@ public class ReactNativePlaidLinkSdkModule: Module {
         // MARK: Default stuff
 
         // Defines event names that the module can send to JavaScript.
-        Events("onChange")
+        Events(ModuleEventName.allCases.map { $0.rawValue })
+
+
 
         // Defines a JavaScript synchronous function that runs the native code on the JavaScript thread.
         Function("hello") {
@@ -91,6 +122,21 @@ public class ReactNativePlaidLinkSdkModule: Module {
             Events("onLoad")
         }
     }
+
+    enum ModuleEventName: String, CaseIterable {
+        case onSuccess
+        case onExit
+        case onEvent
+    }
+
+    enum ModuleFunctionName: String, CaseIterable {
+        case createPlaidLinkSession
+    }
+
+    // MARK: Private
+
+    private var linkSession: PlaidLinkSession?
+    private var sessionCreationError: Error?
 
 //    private class Storage {
 //        // We need a reference to the current session to keep it alive.

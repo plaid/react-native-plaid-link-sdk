a951afee04a6e368a138d670a920bc167c1e833c
dtroupe <dtroupe@plaid.com>
2026-03-25 15:30:35 -0700

    module updates

--- ios/src/ReactNativePlaidLinkSdkModule.swift
+++ ios/src/ReactNativePlaidLinkSdkModule.swift
@@ -23,16 +23,16 @@ public class ReactNativePlaidLinkSdkModule: Module {
 
         AsyncFunction(ModuleFunctionName.createPlaidLinkSession.rawValue) { (token: String, onLoadPromise: Promise) in
             let onSuccess: OnSuccessHandler = { [weak self] success in
-                self?.sendEvent(ModuleEventName.onSuccess.rawValue, ["onSuccess": "fixme"])
+                self?.sendEvent(ModuleEventName.onSuccess.rawValue, success.asDictionary)
             }
 
             let onExit: OnExitHandler = { [weak self] exit in
-                self?.sendEvent(ModuleEventName.onExit.rawValue, ["onExit": "fixme"])
+                self?.sendEvent(ModuleEventName.onExit.rawValue, exit.asDictionary)
                 self?.linkSession = nil
             }
 
             let onEvent: OnEventHandler = { [weak self] event in
-                self?.sendEvent(ModuleEventName.onEvent.rawValue, ["onEvent": event.eventName.description]) // fixme!
+                self?.sendEvent(ModuleEventName.onEvent.rawValue, event.asDictionary)
             }
 
             let onLoad: OnLoadHandler = {
@@ -83,7 +83,7 @@ public class ReactNativePlaidLinkSdkModule: Module {
                 DispatchQueue.main.async {
                     promise.resolve() // not a failure to open, just a miscall
                 }
-                
+
                 return
             }
 
@@ -131,4 +131,151 @@ public class ReactNativePlaidLinkSdkModule: Module {
 
     private var linkSession: PlaidLinkSession?
     private var sessionCreationError: Error?
+}
+
+// MARK: Private Extensions
+
+fileprivate extension Institution {
+    var asDictionary: [String: Any] {
+        return [
+            "name": name ?? "",
+            "id": ID ?? "",
+        ]
+    }
+}
+
+fileprivate extension Account {
+    var asDictionary: [String: Any] {
+        [
+            "id": self.id,
+            "name": self.name,
+            "mask": self.mask ?? "",
+            "subtype": self.subtype.description,
+            "type": subtype.typeName,
+            "verificationStatus": verificationStatus?.description ?? "",
+        ]
+    }
+}
+
+// FIXME: Remove this once type is public on AccountSubtype.
+fileprivate extension AccountSubtype {
+    var typeName: String {
+        switch self {
+        case .other:        return "other"
+        case .credit:       return "credit"
+        case .loan:         return "loan"
+        case .depository:   return "depository"
+        case .investment:   return "investment"
+        case .unknown(let type, _): return type
+        }
+    }
+}
+
+fileprivate extension LinkSuccess {
+    var asDictionary: [String: Any] {
+        [
+            "publicToken": publicToken,
+            "metadata": [
+                "linkSessionId": metadata.linkSessionID,
+                "institution": metadata.institution.asDictionary ?? "",
+                "accounts": metadata.accounts.map { $0.asDictionary },
+                "metadataJson": metadata.metadataJSON ?? "",
+            ]
+        ]
+    }
+}
+
+fileprivate extension LinkExit {
+    var asDictionary: [String: Any] {
+        [
+            "error": error?.asDictionary ?? [:],
+            "metadata": [
+                "status": metadata.status?.description ?? "",
+                "institution": metadata.institution?.asDictionary ?? "",
+                "requestId": metadata.requestID ?? "",
+                "linkSessionId": metadata.linkSessionID ?? "",
+                "metadataJson": metadata.metadataJSON ?? "",
+            ]
+        ]
+    }
+}
+
+fileprivate extension LinkEvent {
+    var asDictionary: [String: Any] {
+        [
+            "eventName": eventName.description,
+            "metadata": metadata.asDictionary,
+        ]
+    }
+}
+
+fileprivate extension EventMetadata {
+    var asDictionary: [String: Any] {
+        [
+            "errorType": errorCode?.errorType ?? "",
+            "errorCode": errorCode?.description ?? "",
+            "errorMessage": errorMessage ?? "",
+            "exitStatus": exitStatus?.description ?? "",
+            "institutionId": institutionID ?? "",
+            "institutionName": institutionName ?? "",
+            "institutionSearchQuery": institutionSearchQuery ?? "",
+            "accountNumberMask": accountNumberMask ?? "",
+            "isUpdateMode": isUpdateMode ?? "",
+            "matchReason": matchReason ?? "",
+            "routingNumber": routingNumber ?? "",
+            "selection": selection ?? "",
+            "linkSessionId": linkSessionID,
+            "mfaType": mfaType?.description ?? "",
+            "requestId": requestID ?? "",
+            "issueId": issueID ?? "",
+            "issueDescription": issueDescription ?? "",
+            "issueDetectedAt": issueDetectedAt.map(iso8601String) ?? "",
+            "timestamp": iso8601String(from: timestamp),
+            "viewName": viewName?.description ?? "",
+            "metadataJson": metadataJSON ?? "",
+        ]
+    }
+}
+
+fileprivate extension ExitStatus {
+    var stringValue: String { self.description } // qwe use this directly!
+}
+
+fileprivate extension MFAType {
+    var stringValue: String { self.description } // qwe use this directly!
+}
+
+fileprivate extension ExitError {
+    var asDictionary: [String: Any] {
+        [
+            "errorType": self.errorCode.description,
+            "errorCode": self.errorCode.errorCodeString,
+            "errorMessage": self.errorMessage,
+            "displayMessage": self.displayMessage ?? "",
+            "errorJson": self.errorJSON ?? "",
+        ]
+    }
+}
+
+fileprivate extension ExitErrorCode {
+    var errorCodeString: String {
+        switch self {
+        case .apiError(let code): return code.description
+        case .authError(let code): return code.description
+        case .assetReportError(let code): return code.description
+        case .internal(let code): return code
+        case .institutionError(let code): return code.description
+        case .itemError(let code): return code.description
+        case .invalidInput(let code): return code.description
+        case .invalidRequest(let code): return code.description
+        case .rateLimitExceeded(let code): return code.description
+        case .unknown(let type, let code): return code
+        }
+    }
+}
+
+private func iso8601String(from date: Date) -> String {
+    let formatter = ISO8601DateFormatter()
+    formatter.formatOptions = [.withInternetDateTime]
+    return formatter.string(from: date)
 }
